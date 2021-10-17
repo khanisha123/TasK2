7aa9f8b7e57ad48a00840c7d5963a69b8754a950
khanisha123 <khanisha@code.edu.az>
2021-10-17 20:50:10 +0400

    Add project files.

---
+++ TasK2/Person.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace TasK2
+{
+    class Person
+    {
+        public string Name { get; set; }
+        public string Surname { get; set; }
+        public int Age { get; set; }
+
+        public Person(string name, string surname, int age)
+        {
+            Name = name;
+            Surname = surname;
+            Age = age;
+        }
+
+        public static bool operator <(Person p1, Person p2)
+        {
+            return p1.Age < p2.Age;
+        }
+        public static bool operator >(Person p1, Person p2)
+        {
+            return p2.Age < p1.Age;
+        }
+    }
+}
---
+++ TasK2/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+
+namespace TasK2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Person p1 = new Person("Khanisha", "Alasgarov", 16);
+            Person p2 = new Person("Husein", "Bagirov", 17);
+            Person p4 = new Person("Kamran", "Esgerov", 25);
+            Person p3 = new Person("Murad", "Memmedov", 20);
+
+            Person[] people = { p2, p1, p4, p3 };
+
+            for (int k = 0; k < people.Length - 1; k++)
+            {
+                if (people[k] > people[k + 1])
+                {
+                    var verify = people[k];
+                    people[k] = people[k + 1];
+                    people[k + 1] = verify;
+                }
+
+            }
+            foreach (var substance in people)
+            {
+                Console.WriteLine(substance.Age);
+            }
+        }
+    }
+}
